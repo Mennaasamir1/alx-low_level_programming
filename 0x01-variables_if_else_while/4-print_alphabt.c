8a456dc800368a247c8f7bc632a23529a171997a
Mennaasamir1 <monyy_20174@outlook.com>
2023-09-13 14:41:35 -0700

    exclude alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: using putchar to print letters
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char	alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		if (alphabet != 'q' && alphabet != 'e')
+			putchar(alphabet);
+
+	return (0);
+}
