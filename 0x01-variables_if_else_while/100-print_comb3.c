c719b0ed875a9d9cafc0778bfe8cc71ddaa7b629
Mennaasamir1 <monyy_20174@outlook.com>
2023-09-15 06:51:34 -0700

    n

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,13 +15,10 @@ int	main(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (i != j)
+			if (i != j && i +  j != 17)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
-			}
-			if (i + j != 17)
-			{
 				putchar(' ');
 				putchar(',');
 			}
