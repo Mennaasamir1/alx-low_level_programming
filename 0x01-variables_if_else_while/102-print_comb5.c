34aa31c809d948b516398e964862a1c0ef16fe1f
Mennaasamir1 <monyy_20174@outlook.com>
2023-09-15 07:48:57 -0700

    m

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: putchar prints a combination of two numbers
+ *
+ * Return: always 0
+ */
+int	main(void)
+{
+	int i,	j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (i = j; j <= 99; j++)
+		{
+			putchar('0' + (i / 10));
+			putchar('0' + (i % 10));
+			putchar(' ');
+			putchar('0' + (j / 10));
+			putchar('0' + (j % 10));
+
+			if (i != 99 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
