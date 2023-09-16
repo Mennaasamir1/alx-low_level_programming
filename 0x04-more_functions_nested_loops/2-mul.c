6ecf55a410f53e98b1dd2cdd344d48e59a6c3518
Mennaasamir1 <monyy_20174@outlook.com>
2023-09-15 19:11:37 -0700

    d

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* multiply - Multiplies two integers
+* @a: The first integer
+* @b: The second integer
+*
+* Return: The product of a and b
+*/
+int	multiply(int a, int b)
+{
+	return (a * b);
+}
