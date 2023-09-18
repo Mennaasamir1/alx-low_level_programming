b0dbf83be9acb0f9bf84c728859eddbfcbbf1664
Mennaasamir1 <monyy_20174@outlook.com>
2023-09-18 06:20:49 -0700

    n

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,13 +9,13 @@
  */
 int _atoi(char *s)
 {
-	unsigned	int	num	=	0;
+	unsigned int	num	=	0;
 	int	sign	=	1;
 
 	do {
 		if (*s == '-')
 			sign *= -1;
-		else if (*s > '0' && *s <= '9')
+		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
 		else if (num > 0)
 			break;
