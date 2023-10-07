1913998e0eb420188dd4fae179f0e7dfb2ed8237
Mennaasamir1 <monyy_20174@outlook.com>
2023-10-07 14:19:00 -0700

    recursion task 3

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of string
+ * @s: pointer to string
+ * Return: 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+
+	return (0);
+}
