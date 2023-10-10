532c220de1cc6db8e692be3d53c4246d98ce0b30
Mennaasamir1 <monyy_20174@outlook.com>
2023-10-10 14:24:53 -0700

    recursion advsion adv 2

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,4 +12,6 @@ int _strlen_recursion(char *s);
   int is_prime_number(int n);
   int is_palindrome(char *s);
   int _checker(char *s, int start, int end);
+int wildcmp(char *s1, char *s2);
+
 #endif
