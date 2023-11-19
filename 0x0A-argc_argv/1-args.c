bc5f4effb83321005a394d8357ac5ccfc90903c0
Mennaasamir1 <monyy_20174@outlook.com>
2023-11-19 12:22:27 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: pointer to an array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
