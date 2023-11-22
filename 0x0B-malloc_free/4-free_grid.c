b64b6470fa61aec072ba0ad5a44458eaa8aa5184
Mennaasamir1 <monyy_20174@outlook.com>
2023-11-22 15:20:11 -0800

    task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees grid
+ * @grid: pointer to array
+ * @height: rows
+ * Return: nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
