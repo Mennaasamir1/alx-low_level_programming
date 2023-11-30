fe751e2e8eb5377e0851b797b18300a8ee9b1ed4
Mennaasamir1 <monyy_20174@outlook.com>
2023-11-30 02:45:08 -0800

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - allocates memory
+ * @b: size of memory allocated
+ */
+void *malloc_checked(unsigned int b)
+{
+
+	int *ptr =  malloc(b);
+
+	if (ptr == 0)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+
+
+}
