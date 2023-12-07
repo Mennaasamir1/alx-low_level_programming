ec0817eb2d7556b1250ce95ae23333ea3f47d3b1
Mennaasamir1 <monyy_20174@outlook.com>
2023-12-07 06:11:49 -0800

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - prints inf about a dog
+ * @name: pointer to name
+ * @age: age of the dog
+ * @owner: pointer to the owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
