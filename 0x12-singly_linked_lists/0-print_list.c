dc880e5744c2b8bd529c3dda8376e1dfab96048b
mennaasamir1 <monyy_20174@outlook.com>
2024-01-20 15:10:39 -0800

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * strlenth - finds the length of the string
+ * @p: pointer to string
+ * Return: length
+ */
+int strlenth(char *p)
+{
+	int i = 0;
+
+	if (!p)
+		return (0);
+	while (*p++)
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * print_list - prints elements of the list
+ * @h: head pointer
+ * Return: count
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", strlenth(h->str), h->str ? h->str : "(nill)");
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
