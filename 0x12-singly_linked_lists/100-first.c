790301367164bfadd8900d8e8ced9643293b9533
mennaasamir1 <monyy_20174@outlook.com>
2024-01-21 14:56:47 -0800

    new task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * myStartupFun - prints a statement
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
