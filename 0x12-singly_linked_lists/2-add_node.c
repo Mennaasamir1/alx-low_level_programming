f3a7ed5d6034a3e6a74f9d951ade16cdf5517528
mennaasamir1 <monyy_20174@outlook.com>
2024-01-21 07:29:57 -0800

    new task

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *add_node - adds a new node
+ * @head: pointer to head pointer
+ * @str: pointer to string
+ * Return: pointer to new node, or NULL on fail
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ptr = malloc(sizeof(list_t));
+	char *s = strdup(str);
+
+	ptr->str = s;
+	ptr->next = NULL;
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		ptr->next = *head;
+		*head = ptr;
+	}
+	return (*head);
+
+}
