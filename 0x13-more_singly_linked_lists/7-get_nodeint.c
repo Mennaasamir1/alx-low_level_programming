c8be7633ca02e27ff78d4ef6a6df098cb7b996fa
mennaasamir1 <monyy_20174@outlook.com>
2024-01-23 07:30:43 -0800

    new task

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * *get_nodeint_at_index -  returns the nth node
+ *@head: head pointer
+ *@index: index of node
+ *
+ * Return: a pointer to the nth node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *ptr = head;
+
+	while (index != 0)
+	{
+		ptr = ptr->next;
+		index--;
+	}
+
+	return (ptr);
+
+}
+
