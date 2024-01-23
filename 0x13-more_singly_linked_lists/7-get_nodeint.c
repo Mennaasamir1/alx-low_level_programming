ab9a3bee576a024c51ca579a1fd959d6c0f775f8
mennaasamir1 <monyy_20174@outlook.com>
2024-01-23 07:32:31 -0800

    new task

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,6 +11,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *ptr = head;
 
+	if (!head)
+	{
+		return (NULL);
+	}
 	while (index != 0)
 	{
 		ptr = ptr->next;
