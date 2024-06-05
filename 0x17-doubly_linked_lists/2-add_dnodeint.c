db25a33c533f87936475727a239dd32388f94ab5
email <monyy_20174@outlool.com>
2024-06-05 07:46:33 -0700

    task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * *add_dnodeint -  adds a new node at the beginning
+ * @head: head pointer
+ * @n: data
+ * Return: a pointer to the new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *temp = malloc(sizeof(dlistint_t));
+
+	temp->prev = NULL;
+	temp->n = n;
+	temp->next = NULL;
+
+	temp->next = *head;
+	*head = temp;
+
+	return (temp);
+
+}
