9f5638bb527e966f1e6835bc3804d87611352a82
email <monyy_20174@outlool.com>
2024-06-07 14:22:33 -0700

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,21 +1,22 @@
 #include "lists.h"
 /**
- * *insert_dnodeint_at_index - adds a node at a given position
- * @h: double pointer
+ * *insert_dnodeint_at_index - inserts a node at a given position
+ * @h: pointer
  * @idx: index
  * @n: data
- * Return: pointer to the new node
+ * Return: a pointer to the new node or NULL
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp = *h, *new;
-	size_t i = 1;
+	dlistint_t *temp = *h, *temp2;
+	dlistint_t *newp = NULL;
+	size_t i;
 
 	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
 	}
-	for (i = 1; i < idx ; i++)
+	for (i = 1; i < idx; i++)
 	{
 		temp = temp->next;
 		if (temp == NULL)
@@ -25,17 +26,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (add_dnodeint_end(h, n));
 	}
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-	{
+	newp = malloc(sizeof(dlistint_t));
+	if (newp == NULL)
 		return (NULL);
-	}
-	new->n = n;
-	new->prev = temp;
-	new->next = temp->next;
-	temp->next->prev = new;
-	temp->next = new;
-
-	return (new);
-
+	newp->n = n;
+	temp2 = temp->next;
+	temp->next = newp;
+	temp2->prev = newp;
+	newp->next = temp2;
+	newp->prev = temp;
 }
